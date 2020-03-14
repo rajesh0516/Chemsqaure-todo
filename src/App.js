4ae65750f5d7934efc116abd9b273ac92bbb6e64
rajesh0516 <30546345+rajesh0516@users.noreply.github.com>
2020-03-15 04:57:17 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { memo } from "react";
+import { useInputValue, useTodos } from "./store";
+import {AddTodo, TodoList}  from "./components";
+
+
+const App = memo(props => {
+  const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
+  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+
+  const clearInputAndAddTodo = _ => {
+    clearInput();
+    addTodo(inputValue);
+  };
+
+  return (
+      <div class="main-container">
+      <AddTodo 
+        inputValue={inputValue}
+        onInputChange={changeInput}
+        onButtonClick={clearInputAndAddTodo}
+        onInputKeyPress={event => keyInput(event, clearInputAndAddTodo)}
+      />
+      <TodoList
+        items={todos}
+        onItemCheck={idx => checkTodo(idx)}
+        onItemRemove={idx => removeTodo(idx)}
+      />
+      </div>
+  );
+});
+
+export default App;
